dcccd17bdd994c166277f9aef0aa31729aeb831e
Hilal Güneş <hilal.gunes@detaysoft.com>
2024-04-16 13:40:47 +0300

    first commit

---
+++ TestingLibrary/build.gradle.kts
@@ -0,0 +1,59 @@
+plugins {
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+    `maven-publish`
+}
+
+android {
+    namespace = "com.hilal.testinglibrary"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+                groupId = "com.github.hilalgunes"
+                artifactId = "TestingLibrary"
+                version = "1.0.0"
+                artifact("$buildDir/outputs/aar/testinglibrary-release.aar")
+
+            }
+        }
+    }
+}
---
+++ TestingLibrary/src/main/java/com/hilal/testinglibrary/test.kt
@@ -0,0 +1,10 @@
+package com.hilal.testinglibrary
+
+/**
+ * Created by Hilal Güneş on 16.04.2024
+ */
+class test {
+    fun testFunction(): String {
+        return "This is a test function"
+    }
+}
